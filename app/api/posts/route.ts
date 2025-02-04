08b38cd6fa281e929be034499a683a97b6f6a58c
yukito <iyggf66974@yahoo.ne.jp>
2025-02-04 12:42:58 +0900

    create ok

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,3 +6,15 @@ export const GET = async (req: Request) =>  {
     const posts = await prisma.post.findMany()
     return NextResponse.json(posts)
 }
+
+export const POST = async (req: Request) =>  {  
+    const { title, content } = await req.json()
+
+    const posts = await prisma.post.create({
+        data: {
+            title,
+            content,
+        }
+    })
+    return NextResponse.json(posts)
+}
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -15,9 +16,13 @@ const formSchema = z.object({
     title: z.string().min(2, {
       message: "Username must be at least 2 characters.",
     }),
-  })
+    content: z.string(),
+})
   
 const CreatePage = () => {
+    const router = useRouter()
+
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -26,10 +31,28 @@ const CreatePage = () => {
         },
       })
 
+     async function onSubmit(values: z.infer<typeof formSchema>) {
+        const { title, content } = values;
+        try {
+            await fetch('http://localhost:3000/api/posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({title, content}),
+            })
+            router.push('/')
+            router.refresh()
+        }  catch (error) {
+            console.log(error);
+            
+        }
+    }
+
   return (
     <>
         <Form {...form}>
-            <form  className="space-y-8">
+            <form  className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
                 <FormField
                 control={form.control}
                 name="title"
